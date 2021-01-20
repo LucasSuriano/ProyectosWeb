ce103a015f24476a140c5b10e024764ac0435cf8
FedericoFortuna <fedekpg17@gmail.com>
2021-01-20 11:52:58 -0300

    Nice Effects

--- js/producto.js
+++ js/producto.js
@@ -1,12 +1,15 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
+    $("#container_nav").fadeOut('normal');
+    $(".openbtn").fadeOut('normal');
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("body").style.marginLeft = "250px";
-    document.getElementById("#hola").style.display = "none";
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
   function closeNav() {
+    $("#container_nav").fadeIn('normal');
+    $(".openbtn").fadeIn('normal');
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("body").style.marginLeft = "0";
   }
