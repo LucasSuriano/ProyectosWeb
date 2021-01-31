8f01875da36be96e2d55c80394925f8a8b324eab
Lucas Suriano <lucassuriano97@gmail.com>
2021-01-30 22:19:43 -0300

    Fix path

--- backend/rutas/inicio.js
+++ backend/rutas/inicio.js
@@ -1,14 +1,13 @@
-const { Router } = require("express");
 const express = require('express');
-const { userInfo } = require("os");
 const router = express.Router();
+const path = require('path');
 
 /*router.get('/', (req, res) => {
     res.render('index.html');
 })*/
 
-router.get('/', function(req,res) {
-    res.sendFile(__dirname + '/index.html');
+router.get('/home', function(req,res) {
+    res.sendFile(path.join(__dirname, '../', '../', 'index.html'));
 })
 
 module.exports = router;
